8eaa13800a76182433a62f806f340fb83718de2f
murilovezao <161845206+murilovezao@users.noreply.github.com>
2024-04-01 12:04:41 -0300

    Add files via upload

---
+++ ARQTEC.sql
@@ -0,0 +1,12 @@
+create database ARQTEC;
+
+use ARQTEC;
+
+CREATE TABLE usuarios(
+id INT UNIQUE NOT NULL PRIMARY KEY,
+nome VARCHAR(255) NOT NULL,
+email VARCHAR(255) UNIQUE NOT NULL,
+idade INT,
+perfilfoto VARCHAR(255)
+);
+   
